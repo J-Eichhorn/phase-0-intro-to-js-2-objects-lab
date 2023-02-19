ad3d7d0e89711755ccb9f31975841e60fca7dd34
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-02-19 13:52:20 -0500

    passes objects lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Josh",
+    streetAddress: "118-80 Metropolitan Ave.",    
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updateEmployee = {...employee};
+
+    updateEmployee[key] = value;
+
+    return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updateEmployee = {...employee[key]};
+    delete updateEmployee.key;
+    return updateEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
